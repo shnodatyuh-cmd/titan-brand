88ff992e3db8d433270eddff3b81ae57824c3f26
shnodatyuh-cmd <shnodatyuh@gmail.com>
2026-08-13 17:41:02 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDIC_YxAHPrXoqF4gGQfTmp8HdKmv4",
+ export const firebaseConfig = {
+  apiKey: "AIzaSyDIC_YxAHFrXoqF4lgQgtFTmp8rHdkRmv4",
   authDomain: "titan-brand.firebaseapp.com",
   projectId: "titan-brand",
   storageBucket: "titan-brand.firebasestorage.app",
   messagingSenderId: "993023688490",
   appId: "1:993023688490:web:b85213fa3f23b67ff0a853",
-  measurementId: "G-6DNZVS455"
+  measurementId: "G-6DZNV0S455"
 };
